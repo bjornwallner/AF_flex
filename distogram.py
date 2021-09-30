d294852b363cf93d749a59377a988f35608705ff
Bjorn Wallner <bjornwallner@gmail.com>
2021-09-30 11:04:27 +0200

    inital commit

---
+++ distogram.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import sys
+from numpy import NaN, Inf, arange, isscalar, asarray, array
+
+def peakdet(v, delta, x = None):
+    """
+    Converted from MATLAB script at http://billauer.co.il/peakdet.html
+    
+    Returns two arrays
+    
+    function [maxtab, mintab]=peakdet(v, delta, x)
+    %PEAKDET Detect peaks in a vector
+    %        [MAXTAB, MINTAB] = PEAKDET(V, DELTA) finds the local
+    %        maxima and minima ("peaks") in the vector V.
+    %        MAXTAB and MINTAB consists of two columns. Column 1
+    %        contains indices in V, and column 2 the found values.
+    %      
+    %        With [MAXTAB, MINTAB] = PEAKDET(V, DELTA, X) the indices
+    %        in MAXTAB and MINTAB are replaced with the corresponding
+    %        X-values.
+    %
+    %        A point is considered a maximum peak if it has the maximal
+    %        value, and was preceded (to the left) by a value lower by
+    %        DELTA.
+    
+    % Eli Billauer, 3.4.05 (Explicitly not copyrighted).
+    % This function is released to the public domain; Any use is allowed.
+    
+    """
+    maxtab = []
+    mintab = []
+       
+    if x is None:
+        x = arange(len(v))
+    
+    v = asarray(v)
+    
+    if len(v) != len(x):
+        sys.exit('Input vectors v and x must have same length')
+    
+    if not isscalar(delta):
+        sys.exit('Input argument delta must be a scalar')
+    
+    if delta <= 0:
+        sys.exit('Input argument delta must be positive')
+    
+    mn, mx = Inf, -Inf
+    mnpos, mxpos = NaN, NaN
+    
+    lookformax = True
+    
+    for i in arange(len(v)):
+        this = v[i]
+        if this > mx:
+            mx = this
+            mxpos = x[i]
+        if this < mn:
+            mn = this
+            mnpos = x[i]
+        
+        if lookformax:
+            if this < mx-delta:
+                maxtab.append((mxpos, mx))
+                mn = this
+                mnpos = x[i]
+                lookformax = False
+        else:
+            if this > mn+delta:
+                mintab.append((mnpos, mn))
+                mx = this
+                mxpos = x[i]
+                lookformax = True
+
+    return array(maxtab), array(mintab)
+
+
+# In[9]:
+
+
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+
+def softmax(x):
+    f_x = np.exp(x) / np.sum(np.exp(x))
+    return f_x
+
+pickle_file='result_model_1_msas1_chainbreak_offset200_recycles3_1.pkl'
+results=pickle.load(open(pickle_file,'rb'))
+print(results['distogram'].keys())
+bin_edges=results['distogram']['bin_edges']
+bin_size=bin_edges[1]-bin_edges[0]
+#convert the bin_edges to bin_centers
+x=bin_edges+bin_size/2
+#Add the first bin center to the begining of x to complete the conversion
+first_bin=bin_edges[0]-bin_size/2
+x=np.insert(x,0,first_bin)
+
+distogram=results['distogram']['logits']
+print(results['distogram']['logits'].shape)
+print(len(x))
+#Took a random position that I know was in contact just to get something
+pos1=160
+pos2=100
+y=distogram[pos1-1][pos2-1]
+#the distogram are logits and need to be converted to probablity using softmax
+#
+prob=softmax(y)
+plt.plot(x,prob)
+#plt.plot(x,y_S)
+plt.show()
+(maxima,minima)=peakdet(prob,0.03,x)
+maxima
+
+
+# In[12]:
+
+
+#Example Search for all maximas
+N=distogram.shape[0] #this is the length of the pdb
+
+for i in range(N):
+    for j in range(N):
+        if j<=i: #skipping symmetric pairs
+            continue
+        prob=softmax(distogram[i][j])
+        (maxima,minima)=peakdet(prob,0.03,x)
+        n=len(maxima)
+        if(n >0):
+            print(f'Found {n} maxima for pair {i+1},{j+1}: {maxima}')
+        #print(len(maxima))
+        #print(maxima)
+
